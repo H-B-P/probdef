52484576e4b5a22d35e63090d320adafbc35035c
H-B-P <pointy@protonmail.ch>
2017-08-07 14:12:46 +0100

    take two

---
+++ android/src/com/hbp/probdef/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.hbp.probdef;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.hbp.probdef.ProbDef;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ProbDef(), config);
+	}
+}
---
+++ desktop/src/com/hbp/probdef/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.hbp.probdef.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.hbp.probdef.ProbDef;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ProbDef(), config);
+	}
+}
